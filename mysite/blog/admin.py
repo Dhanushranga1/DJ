a21cee36e9fa003c3829917c5093576de2ed9b6e
Dhanushranga1 <dg1513@srmist.edu.in>
2024-10-25 22:14:49 +0530

    added the Comment class for the admin module

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import Comment, Post
 # Register your models here.
 
 from .models import Post
@@ -13,3 +13,9 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
     list_display = ['title', 'slug','author','publish', 'status']
     show_facets= admin.ShowFacets.ALWAYS
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
