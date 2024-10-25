bdaa1f07e7a5f2eb639cf29d62227f3d9a380118
Dhanushranga1 <dg1513@srmist.edu.in>
2024-10-25 22:07:14 +0530

    created the post_comment()

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,50 @@
+from msilib.schema import ListView
+
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from .forms import EmailPostForm, CommentForm
 # Create your views here.
 from .models import Post
+from django.core.mail import send_mail
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False  # Initialize the variable to avoid UnboundLocalError
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True  # Set sent to True after sending the email
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent  # Always pass the 'sent' variable, even if no email was sent
+        }
+    )
+
 
 def post_list(request):
     post_list = Post.published.all()
@@ -36,3 +78,31 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',{
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
+
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    # this is an alternative list view for our posts
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
